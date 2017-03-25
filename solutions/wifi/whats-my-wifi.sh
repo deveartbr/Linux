69047409a2a17d1c97feed16d46f2cfadcca5b45
TotaBraz Deveart.com <deveartbr@users.noreply.github.com>
2017-03-25 14:16:22 -0300

    Create whats-my-wifi.sh

---
+++ solutions/wifi/whats-my-wifi.sh
@@ -0,0 +1,10 @@
+TEST=0
+cd /etc/NetworkManager/system-connections/
+echo "Saved wifi list:"
+echo ""
+ls -l -a
+echo "Type the wifi's name:"
+sudo cat 
+
+echo "the passwors key will be as:"
+echo "\"psk\"=\"PASSWORD\". "
