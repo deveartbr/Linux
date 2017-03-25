9f73a8e03551a58d2dff7dc4609ae77627720063
TotaBraz Deveart.com <deveartbr@users.noreply.github.com>
2017-03-25 14:37:07 -0300

    Update whats-my-wifi.sh

--- solutions/wifi/whats-my-wifi.sh
+++ solutions/wifi/whats-my-wifi.sh
@@ -1,10 +1,19 @@
+#just to active the sudo passwd
 sudo clear
+
 cd /etc/NetworkManager/system-connections/
+
 echo "Saved wifi list:"
 echo ""
 ls -l -a
-echo "Type the wifi's name:"
-sudo cat 
 
+echo ""
+echo ""
+echo "Type the wifi's name:"
+read WIFI_NAME
+sudo cat "$WIFI_NAME"
+
+echo ""
+echo "!!!Atention!!!"
 echo "the passwors key will be as:"
 echo "\"psk\"=\"PASSWORD\". "
